e12c1a8960dd8b03e427248044479ea737b9e5b5
CEDMD\crme008 <marina.burlacu@iongroup.com>
2023-02-22 15:18:24 +0000

    app.module change

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -3,24 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
 import { NgbModule }  from '@ng-bootstrap/ng-bootstrap';
 import { GroupListComponent } from './components/group-list/group-list.component';
-
 import { ModalComponent } from './components/modal/modal.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import {AppRoutingModule} from './app.routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FilterGroupsPipe } from './pipes/filter-groups.pipe';
-
 import { HttpClientModule } from '@angular/common/http';
 import { DecimalPipe } from '@angular/common';
-
-
-
-
 
 @NgModule({
   declarations: [
